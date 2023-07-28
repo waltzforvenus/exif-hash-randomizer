48a812f01f22a40991aa477fc0189151850aaecb
waltzforvenus <mert@autistici.org>
2023-07-29 00:10:49 +0100

    randomize 4 pixels

--- __init__.py
+++ __init__.py
@@ -1,7 +1,19 @@
 import sys
 import os
+import random
 
 from PIL import Image
+
+def randomize_a_pixel(img):
+    
+    # get a random number between 0 and 255
+    c = lambda: random.randint(0,255)
+    
+    x = random.randint(0, img.size[0])
+    y = random.randint(0, img.size[1])
+    
+    img.putpixel((x,y), (c(), c(), c()))
+    
 
 def remove_exif_and_randomize_hash(image_path):
     img = Image.open(image_path)
@@ -9,6 +21,9 @@ def remove_exif_and_randomize_hash(image_path):
     # extend a tiny bit of the image to randomize the hash
     image_dimensions = img.size
     img.resize(image_dimensions[0] + int(image_dimensions[0]/100), image_dimensions[1] + int(image_dimensions[1]/100))
+    
+    # change 5 pixels of the image to add some noise
+    [randomize_a_pixel(img) for i in range(5)]
     
     # save the image and remove exif data
     img.save(image_path, exif=b'')
